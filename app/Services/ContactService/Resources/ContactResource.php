802294a9f40ea3b32d2d1fdadc3db1e3b79841bb
Abbas Momeni <abbasmomeny1994@gmail.com>
2023-03-06 16:19:51 +0330

    fix

--- app/Services/ContactService/Resources/ContactResource.php
+++ app/Services/ContactService/Resources/ContactResource.php
@@ -7,23 +7,27 @@ use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
+/**
+ * @property int $id
+ * @property int $satisfaction_rate
+ * @property string $comment
+ * @property string $called_number
+ */
 class ContactResource extends JsonResource
 {
 
     public function toArray(Request $request): array
     {
-        // dd($this->id  ,$this->comment , $this->status_id );
-        // dd($this , $this->id , $this->status_id );
         return [
             'id'                => $this->id,
             'customer_id'       => $this->customer_id ? new CustomerResource($this->customer) : 'no identifed',
             'status_id'         => new StatusResource($this->status),
             'category_id'       => $this->category_id ? new CategoryResource($this->category) : ' no category',
             'operator_id'       => $this->operator_id ? new OperatorResource($this->operator) : 'no operator',
-            'satisfaction_rate' => 4,
+            'satisfaction_rate' => $this->satisfaction_rate,
             'duration'          => Carbon::createFromTime(0, 0, 10)->toTimeString(),
-            'comment'           => 'this comment',
-            'called_number'     => '03648897987'
+            'comment'           => $this->comment,
+            'called_number'     => $this->called_number
         ];
     }
 }
