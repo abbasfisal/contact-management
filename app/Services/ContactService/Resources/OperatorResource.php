6719eb6c24573bb9d886371d37cbea70674848bd
Abbas Momeni <abbasmomeny1994@gmail.com>
2023-03-06 15:41:57 +0330

    fix:add phpdoc prpoerty annotation for resources

--- app/Services/ContactService/Resources/OperatorResource.php
+++ app/Services/ContactService/Resources/OperatorResource.php
@@ -4,14 +4,18 @@ namespace App\Services\ContactService\Resources;
 
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Support\Collection;
 
+/**
+ * @property int $id ,
+ * @property string $first_name ,
+ * @property string $last_name ,
+ * @property Collection $categories ,
+ *
+ */
 class OperatorResource extends JsonResource
 {
-    /**
-     * Transform the resource into an array.
-     *
-     * @return array<string, mixed>
-     */
+
     public function toArray(Request $request): array
     {
 
